c38e040ef0667441c0c2bdd8257c273b2f83152c
futug <102650930+futug@users.noreply.github.com>
2023-04-29 08:59:54 +0600

    gallery show more buttn without__adaptive

---
+++ script.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".gallery__show-more-btn"),
+  cards = Array.from(document.querySelectorAll(".gallery__item"));
+
+function openCatalog() {
+  btn.addEventListener("click", () => {
+    cards.forEach((item) => item.classList.remove("hidden"));
+    btn.classList.add("hidden");
+  });
+}
+function responseFirst() {
+  if (window.innerWidth > 1174) {
+    btn.classList.add("hidden");
+    cards.forEach((item, index) => {
+      item.classList.add("hidden");
+      if (index <= 5) {
+        item.classList.remove("hidden");
+      } else if (index > 5) {
+        btn.classList.remove("hidden");
+      }
+      openCatalog();
+    });
+  }
+}
+responseFirst();
