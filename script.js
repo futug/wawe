0ab957435ccdf691315eb53423c2128736fe5c9f
futug <102650930+futug@users.noreply.github.com>
2023-05-01 22:54:19 +0600

    video finished, map finished, validat&gallery wait

--- script.js
+++ script.js
@@ -256,7 +256,8 @@ videoButton.addEventListener("click", () => {
 function createIframe() {
   let iFrame = document.createElement("iframe");
   iFrame.setAttribute("allowfullscreen", "");
-  iFrame.setAttribute("src", "https://www.youtube.com/embed/TeU3o12U2hM");
+  iFrame.setAttribute("src", "https://www.youtube.com/embed/TeU3o12U2hM?autoplay=1");
+  iFrame.setAttribute("allow", "autoplay");
   iFrame.classList.add("video__cover");
   return iFrame;
 }
@@ -264,11 +265,12 @@ function createIframe() {
 //!!VIDEO FRAME ENDS!!//
 
 let map;
-
 function initMap() {
+  const centerPosition = { lat: 29.025885, lng: -13.602853 };
+  const mainPosition = { lat: 29.12960607069736, lng: -13.60995008872115 };
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: centerPosition,
+    zoom: 11,
     styles: [
       {
         featureType: "all",
@@ -439,6 +441,22 @@ function initMap() {
         ],
       },
     ],
+    disableDefaultUI: true,
+  });
+  const svgMarker = {
+    path: "M28.3962 70C28.3962 70 56.7925 44.079 56.7925 28.3962C56.7925 12.7134 44.079 0 28.3962 0C12.7134 0 0 12.7134 0 28.3962C0 44.079 28.3962 70 28.3962 70Z",
+    fillColor: "#00B2A0",
+    fillOpacity: 1,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 1,
+    anchor: new google.maps.Point(30, 60),
+  };
+  new google.maps.Marker({
+    position: mainPosition,
+    map,
+    title: "Hello World!",
+    icon: svgMarker,
   });
 }
 
